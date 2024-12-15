f52ae6a18add09c991ae8f06fdfd32c2d02be870
ivan.ragac032@gmail.com <ivan.ragac032@gmail.com>
2024-12-15 16:55:06 +0100

    pocetak

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        IzvedenaKlasa objekt = new IzvedenaKlasa();
+        objekt.PrikaziPoruku();
+    }
+    public class BaznaKlasa
+    {
+        public virtual void PrikaziPoruku()
+            {
+            Console.WriteLine("pero");
+            }
+
+    }
+    public class IzvedenaKlasa : BaznaKlasa
+    {
+
+    }
+
+}
